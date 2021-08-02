a8616fc2d2c7c00bf73a81bc7640d6d7220ab5a8
kondov <deniskondov7@gmail.com>
2021-08-02 20:28:57 +0300

    Setup.py changed to default condition

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import setuptools
 from convertor import __version__
 
 setuptools.setup(
-    name='leet_test_task_decode',
+    name='leet-tools',
     version=__version__,
     description='This is a test leet task',
     zip_safe=False,
     include_package_data=True,
-    packages=setuptools.find_packages('convertor'),
+    packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
-            'leet_test_task_decode=convertor.app:decode'
+            # TODO: need to implement this functionality [6]
         ]
     },
 )
